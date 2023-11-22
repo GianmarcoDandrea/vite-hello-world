af60ff25221b51f6ca4e08923b6ab94897fbd872
GianmarcoDandrea <gianm96@hotmail.it>
2023-11-22 15:04:34 +0100

    update: complete exercise

---
+++ src/components/AppTitle.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+  data() {
+    return {
+      textmessage: "La mia prima app con Vite!",
+    };
+  },
+};
+</script>
+
+<template>
+  <h1>{{ textmessage }}</h1>
+</template>
+
+<style scoped>
+</style>
